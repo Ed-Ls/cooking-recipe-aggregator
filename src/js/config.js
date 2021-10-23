54ef3eea3824ab6ed8adf8620dbdcbe3574dd96a
EdCh-Lo <chloe.louis96@gmail.com>
2021-10-23 19:53:34 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "d8e0c00f-6aa7-45f5-8c77-b176f7b9f67c";
+export const MODAL_CLOSE_SEC = 2.5;
